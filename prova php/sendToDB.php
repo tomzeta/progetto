9481e3dfe63e3dcf77788937d2b0b5651d88032a
tomzeta <tommy.zambon@gmail.com>
2017-09-01 19:18:38 +0200

    php

---
+++ prova php/sendToDB.php
@@ -0,0 +1,25 @@
+﻿<?php 
+    include_once "dbConnect.php";
+    
+    function test_input($data) {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+    
+    if(isset($_POST["postNews"])) {
+        $titolo = $_POST["title"];
+        $testo = $_POST["testo"];
+        
+        $query = "INSERT INTO News(id, titolo, testo) VALUES (,'$titolo','$testo')";
+        $result = $conn->query($query);
+        if($result) echo "Hai pubblicato con successo";
+        
+    }
+    
+    echo "</br>";
+    echo $titolo;
+    echo $testo;
+    
+?>
