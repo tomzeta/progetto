53d2f307e871040aec366affa55b4eea4cf1d326
Leonardo Bragagnolo <leo.braga95@gmail.com>
2017-09-02 18:09:59 +0200

    diviso js da html

---
+++ menu_button.js
@@ -0,0 +1,9 @@
+var div = document.getElementById('click-content');
+    if (div.style.display !== 'none') {
+        div.style.display = 'none';
+    }
+    else {
+        div.style.display = 'block';
+    }
+
+                
