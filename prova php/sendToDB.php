b06767b06ad4f1d073c5537cc807e4b6e884d10d
tomzeta <tommy.zambon@gmail.com>
2017-09-02 16:26:42 +0200

    Php base che in teoria funziona

--- prova php/sendToDB.php
+++ prova php/sendToDB.php
@@ -1,25 +1,17 @@
 ﻿<?php 
-    include_once "dbConnect.php";
-    
-    function test_input($data) {
-        $data = trim($data);
-        $data = stripslashes($data);
-        $data = htmlspecialchars($data);
-        return $data;
+    require 'dBconnect.php';
+    $conn = Connect();
+
+    $titolo = $_POST['title'];
+    $testo = $_POST['testo'];
+
+    $query = "INSERT into News(titolo, testo) VALUES('" . $titolo . "','" . $testo . "')";
+    $success = $conn->query($query);
+    if(!$success){
+        die("Non si possono inserire i dati: ".$conn->error);
     }
-    
-    if(isset($_POST["postNews"])) {
-        $titolo = $_POST["title"];
-        $testo = $_POST["testo"];
-        
-        $query = "INSERT INTO News(id, titolo, testo) VALUES (,'$titolo','$testo')";
-        $result = $conn->query($query);
-        if($result) echo "Hai pubblicato con successo";
-        
-    }
-    
-    echo "</br>";
-    echo $titolo;
-    echo $testo;
-    
+
+    echo "Grazie per aver inserito i dati";
+
+    $conn->close();
 ?>
