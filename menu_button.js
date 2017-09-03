85c0ab58e72dc5d0f4dccb152be2e1644c5fa4dd
Leonardo Bragagnolo <leo.braga95@gmail.com>
2017-09-03 15:19:59 +0200

    cc

--- menu_button.js
+++ menu_button.js
@@ -1,9 +1,9 @@
-var div = document.getElementById('click-content');
+function showmenu(){
+    var div = document.getElementById('click-content');
     if (div.style.display !== 'none') {
         div.style.display = 'none';
     }
     else {
         div.style.display = 'block';
     }
-
-                
+}
